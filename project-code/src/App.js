bbff39b4cb842343f7c7b075a91ac65bee9d9799
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2022-11-29 16:34:08 -0800

    new changes to nav

--- project-code/src/App.js
+++ project-code/src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
-import Header from './Components/Header/Header';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import SimpleBottomNavigation from "./Components/BottomNav/BottomNav";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Trending from "./Pages/Trending/Trending";
+import Search from "./Pages/Search/Search";
+import { Container } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
-    </div>
+      <div className="app">
+        <Routes>
+          <Route path="/" component={Trending} exact />
+          <Route path="/movies" component={Movies} />
+          <Route path="/series" component={Series} />
+          <Route path="/search" component={Search} />
+        </Routes>
+      </div>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
   );
 }
 
