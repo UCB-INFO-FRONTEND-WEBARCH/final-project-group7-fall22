f6a9889d8689faa0fba5604a832852f26f74b8d5
NING ZHANG <ningzhang@NINGs-MacBook-Pro.local>
2022-12-01 17:44:30 -0800

    added top navigtaion

---
+++ project-code/src/Components/TopNav/TopNav.js
@@ -0,0 +1,29 @@
+import { Link} from "react-router-dom";
+import "./TopNav.css";
+import TvIcon from "@material-ui/icons/Tv";
+import MovieIcon from "@material-ui/icons/Movie";
+import SearchIcon from "@material-ui/icons/Search";
+import WhatshotIcon from "@material-ui/icons/Whatshot";
+
+function TopNav() {
+    return (
+        <div className="top-nav">
+            <ul className="top-nav-ul">
+                <li className="top-nav-li">
+                    <Link to="/"><WhatshotIcon className="icon"/>Trending</Link>
+                </li>
+                <li className="top-nav-li">
+                    <Link to="/movies"><MovieIcon className="icon"/>Movies</Link>
+                </li>
+                <li className="top-nav-li">
+                    <Link to="/series"><TvIcon className="icon"/>TV Series</Link>
+                </li>
+                <li className="top-nav-li">
+                    <Link to="/search"><SearchIcon className="icon"/>Search</Link>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default TopNav;
